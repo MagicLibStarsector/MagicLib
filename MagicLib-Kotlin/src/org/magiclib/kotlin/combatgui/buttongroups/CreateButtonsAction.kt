9330ea94a58ac058033a852f3d097f259dd82b88
DesperatePeter <jannes@eckart-ahrensburg.de>
2023-06-15 22:31:03 +0200

    feat: add combat GUI lib

---
+++ MagicLib-Kotlin/src/org/magiclib/kotlin/combatgui/buttongroups/CreateButtonsAction.kt
@@ -0,0 +1,12 @@
+package org.magiclib.kotlin.combatgui.buttongroups
+
+/**
+ * Implement this interface to create buttons for a button group
+ * Consider using CreateSimpleButtons rather than creating your own action
+ */
+interface CreateButtonsAction {
+    /**
+     * call group.addButton in this method to add a button to the group
+     */
+    fun createButtons(group: DataButtonGroup)
+}
---
+++ MagicLib-Kotlin/src/org/magiclib/kotlin/combatgui/buttons/DataToggleButton.kt
@@ -0,0 +1,24 @@
+package org.magiclib.kotlin.combatgui.buttons
+
+/**
+ * A Button that can be activated/deactivated by clicking on it
+ * Used in button groups, don't use directly unless you know what you are doing
+ */
+class DataToggleButton(
+    val data: Any, info: ButtonInfo
+) : ButtonBase(info) {
+    override fun advance(): Boolean {
+        if (isClicked()) {
+            isActive = !isActive
+            return true
+        }
+        return false
+    }
+
+    /**
+     * returns data set in ctr if active, null otherwise
+     */
+    fun getDataIfActive(): Any? {
+        return if (isActive) data else null
+    }
+}
