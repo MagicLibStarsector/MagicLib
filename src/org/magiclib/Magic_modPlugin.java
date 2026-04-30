77a792586d56f71ea807ac1c48ac7578dcb91ecf
S-Numan <TheSadNumanator@gmail.com>
2026-04-30 08:30:29 -0700

    missed import

--- src/org/magiclib/Magic_modPlugin.java
+++ src/org/magiclib/Magic_modPlugin.java
@@ -6,6 +6,7 @@ import com.fs.starfarer.api.Global;
 import com.fs.starfarer.api.campaign.SectorAPI;
 import com.thoughtworks.xstream.XStream;
 import org.magiclib.paintjobs.appliers.MagicPaintjobCampaignApplier;
+import org.json.JSONException;
 import org.magiclib.util.memberMemory.MemberMemoryManager;
 import org.lwjgl.util.vector.Vector2f;
 import org.magiclib.achievements.MagicAchievementManager;
