2ea01bc12d510070163f2b8afa14e475af7d0340
S-Numan <TheSadNumanator@gmail.com>
2026-08-27 07:35:23 -0700

    Externalize string

--- src/org/magiclib/bounty/intel/MagicBountyInfo.kt
+++ src/org/magiclib/bounty/intel/MagicBountyInfo.kt
@@ -596,7 +596,7 @@ open class MagicBountyInfo(val bountyKey: String, val bountySpec: MagicBountySpe
         val deadline = bountySpec.job_deadline
         if(deadline > 0 && MagicBountyCoordinator.getInstance().deadlinesEnabled) {
             textTooltip.addPara(
-                "This bounty expires $deadline days after acceptance", // TODO, externalize string
+                MagicTxt.getString("mb_descDeadline").format(deadline),
                 2f,
                 Misc.getHighlightColor(),
                 deadline.toString()
